dc4fbfa9ee08b4a20abb3d27517ffaa33d9682f9
Terry Bu <burea1124@gmail.com>
2015-03-19 17:48:12 -0400

    extracted html out of the result page - just need to pull out the times from the HTML string now through string parsing

--- WebViewController.m
+++ WebViewController.m
@@ -12,25 +12,37 @@
 
 @end
 
+static NSString* const kFormPageURL = @"http://nadir.nilu.no/cgi-bin/olaeng/VitD-ez_quartMEDandMED_v2.cgi";
+static NSString* const kResultPageURL = @"http://nadir.nilu.no/cgi-bin/olaeng/VitD-ez_quartMEDandMED_v2.cgi";
+
 @implementation WebViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [self.webView loadRequest:[NSURLRequest requestWithURL: [NSURL URLWithString:@"http://nadir.nilu.no/~olaeng/fastrt/VitD-ez_quartMEDandMED_v2.html"]]];
+    [self.webView loadRequest:[NSURLRequest requestWithURL: [NSURL URLWithString:kFormPageURL]]];
     self.webView.delegate = self;
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
     
-    if ([webView.request.URL.absoluteString isEqualToString:@"http://nadir.nilu.no/~olaeng/fastrt/VitD-ez_quartMEDandMED_v2.html"]){
+    if ([webView.request.URL.absoluteString isEqualToString:kFormPageURL]){
         [self.webView stringByEvaluatingJavaScriptFromString:@"document.getElementsByName(\"skin_index\")[1].checked = true"];
         [self.webView stringByEvaluatingJavaScriptFromString:@"var inputs = document.getElementsByTagName('input'); for(var i = 0; i < inputs.length; i++) {if(inputs[i].type.toLowerCase() == 'submit') {inputs[i].click();}}"];
     }
     
-    if ([webView.request.URL.absoluteString isEqualToString:@"http://nadir.nilu.no/cgi-bin/olaeng/VitD-ez_quartMEDandMED_v2.cgi"])
-        NSLog(@"found the submit result page");
+    if ([webView.request.URL.absoluteString isEqualToString:kResultPageURL]) {
+        NSLog(@"found the result page");
+        NSError *error;
+        NSString *resultPageHTML = [NSString stringWithContentsOfURL:[NSURL URLWithString:kResultPageURL]
+                                                            encoding:NSASCIIStringEncoding
+                                                               error:&error];
+        if (!error)
+            NSLog(resultPageHTML);
+        else
+            NSLog(error.description);
+    }
 
 }
 
